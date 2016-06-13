968a2f464c81ba7917ed845bb87aee6667d3a7fb
Sean Teare <steare573@gmail.com>
2016-06-13 08:59:20 -0400

    alter schema to change many to many relationship between league and draft to many to one

--- models/Draft.js
+++ models/Draft.js
@@ -8,6 +8,22 @@ module.exports = function(sequelize, DataTypes) {
       primaryKey: true,
       autoIncrement: true
     },
+    LeagueId: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+      references: {
+        model: 'League',
+        key: 'Id'
+      }
+    },
+    SeasonId: {
+      type: DataTypes.INTEGER(5),
+      allowNull: true,
+      references: {
+        model: 'Season',
+        key: 'Id'
+      }
+    },
     StartDateTime: {
       type: DataTypes.DATE,
       allowNull: true
